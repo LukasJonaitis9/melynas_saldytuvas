a9c3feffbdd4c0462e7eef905231bacadc358a0b
Your Namehehehehe <dom.jure.jad@gmail.com>
2024-01-03 13:45:34 +0200

    beveik pabaigtas saldytuvas, tik vps neveikia

--- blue_frieza.py
+++ blue_frieza.py
@@ -1,4 +1,26 @@
+#def saldytuvo_turinys(saldytuve_yra):
 saldytuve_yra = {'Duona': 1.6, 'Sūris': 0.5, 'Grietinė': 0.7, 'Sūrelis': 5, 'Sviestas': 0.32}
-print(saldytuve_yra)
 
-def saldytuvo_turinys()
+def prideti_produkta(saldytuve_yra, produktas, kiekis):
+        saldytuve_yra[produktas, kiekis] = input("Ko ir kiek pridėsite?:")
+        if produktas in saldytuve_yra:
+            if saldytuve_yra[produktas] >= kiekis:
+                saldytuve_yra[produktas] += kiekis
+                print(f"{produktas} pridėtas prie šaldytuvo. Naujas kiekis: {saldytuve_yra[produktas]}")
+            else:
+                saldytuve_yra[produktas] = kiekis
+
+def atimti_produkta(saldytuve_yra, produktas, kiekis):
+        saldytuve_yra[produktas, kiekis] = input("Ko ir kiek išimsite?:")
+        if produktas in saldytuve_yra:
+            if saldytuve_yra[produktas] >= kiekis:
+                saldytuve_yra[produktas] -= kiekis
+                print(f"{produktas} išimtas iš šaldytuvo. Naujas kiekis: {saldytuve_yra[produktas]}")
+            else:
+                print(f"Produkto {produktas} kiekis nepakankamas.")
+        else:
+            print(f"Produkto {produktas} nėra.\nLaikas eiti į parduotuvę.")
+
+for produktas, kiekis in saldytuve_yra.items():
+    print(f'{produktas}: {kiekis} vnt.')
+    #def kiekio_patikrinimas(saldytuve_yra, produktas, kiekis):
