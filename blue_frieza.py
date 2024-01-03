50b2015deee2fd37be416dcafeb797846561c10a
Your Namehehehehe <dom.jure.jad@gmail.com>
2024-01-03 11:48:10 +0200

    saldytuvo produktu sarasas

--- blue_frieza.py
+++ blue_frieza.py
@@ -1,2 +1,4 @@
-def fridge_products(fridge_content, product_amount):
-    print('Welcome to the Frieza')
+saldytuve_yra = {'Duona': 1.6, 'Sūris': 0.5, 'Grietinė': 0.7, 'Sūrelis': 5, 'Sviestas': 0.32}
+print(saldytuve_yra)
+
+def saldytuvo_turinys()
